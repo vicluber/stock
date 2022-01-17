6f258ffb81a94db278318b161881cecfa82ff93f
Victor Willhuber <victorwillhuber@gmail.com>
2022-01-17 11:25:36 +0100

    [FEATURE] Setting database for Brands and Suppliers
    - php artisan make:model -a has created also Policies and Requests

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Product::factory()->count(20)->create();
         \App\Models\Category::factory()->count(10)->create();
         \App\Models\Item::factory()->count(150)->create();
+        //\App\Models\Supplier::factory()->count(15)->create();
+        //\App\Models\Brand::factory()->count(30)->create(); SupplierSeeder.php exist just and example for seeding tables in different classes with this command: php artisan db:seed --class=BrandSeeder
     }
 }
