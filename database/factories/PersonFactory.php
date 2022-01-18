133a4b3e45227e8c7d1060705a6084d14f4964d0
Victor Willhuber <victorwillhuber@gmail.com>
2022-01-18 15:04:04 +0100

    [FEATURE] Adding Person mcr, user-person relationship and person factory

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'phone' => $this->faker->e164PhoneNumber(),
+            'birth_date' => $this->faker->date($format = 'Y-m-d', $min = 'now'),
+        ];
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('person_id')->unsigned()->index();
+            $table->foreign('person_id')->references('id')->on('people')->onDelete('cascade');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
